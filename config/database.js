a20edbbca2977ea09ebc5bb842e4208864a40832
pranathi1923 <pranathireddymkl@gmail.com>
2021-07-05 18:42:28 +0530

    first commit

---
+++ config/database.js
@@ -0,0 +1,5 @@
+const { DB_USERNAME,DB_PASSWORD,DB_CLUSTER } = require("./config");
+
+module.exports = {
+  MongoURI: `mongodb+srv://pranathi:charitha1983@cluster0.ndami.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+};
